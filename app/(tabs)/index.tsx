d67caf77052cfac13fe11714c9dd0d6633f4c24a
Fernando <fernandocassiodev@gmail.com>
2024-12-09 21:45:13 -0300

    feat: text color class added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className={"text-red-500"}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
